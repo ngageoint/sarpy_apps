3b89fbd437847d9e683524ff7dbc1bdc47e2a1e1
Jason Casey <j.casey@beamio.net>
2020-09-21 12:48:23 -0400

    update to deskew demo

--- examples/deskew/deskew.py
+++ examples/deskew/deskew.py
@@ -4,23 +4,81 @@ import matplotlib.pyplot as plt
 import sarpy.visualization.remap as remap
 from sarpy.processing.aperture_filter import ApertureFilter
 
-dim = 0
-
 fname = os.path.expanduser("~/sicd_example_RMA_RGZERO_RE16I_IM16I.nitf")
 reader = sarpy_complex.open(fname)
-selected_region_image_coords = [3522.497816593887, 5701.362445414848, 3909.85152838428, 6088.716157205241]
-complex_data = reader[3522:3900, 5701:6088]
+
+y_start = 5100
+y_end = 5500
+x_start = 5200
+x_end = 5600
+
+complex_data = reader[y_start:y_end, x_start:x_end]
 complex_display = remap.density(complex_data)
 
+print("Image subsection")
+plt.imshow(complex_display)
+plt.show()
 
-aperture_filter = ApertureFilter(reader, dimension=dim)
-aperture_filter.apply_deweighting = True
-aperture_filter.set_sub_image_bounds((3522, 3900), (5701, 6088))
-filtered_image = aperture_filter[:]
-complex_image = aperture_filter.normalized_phase_history
-complex_display = remap.density(complex_image)
+aperture_filter = ApertureFilter(reader, dimension=0)
+aperture_filter.set_sub_image_bounds((y_start, y_end), (x_start, x_end))
+aperture_filter.apply_deweighting = False
+phase_data = aperture_filter.normalized_phase_history
+phase_display = remap.density(phase_data)
+filtered_image = aperture_filter[:, 0:100]
 filtered_display = remap.density(filtered_image)
 
+print("phase data, no deweighting, dim=0")
+plt.imshow(phase_display)
+plt.show()
 
-plt.imshow(complex_display)
+print("filtered image")
+plt.imshow(filtered_display)
+plt.show()
+
+aperture_filter = ApertureFilter(reader, dimension=0)
+aperture_filter.set_sub_image_bounds((y_start, y_end), (x_start, x_end))
+aperture_filter.apply_deweighting = True
+phase_data = aperture_filter.normalized_phase_history
+phase_display = remap.density(phase_data)
+filtered_image = aperture_filter[:, 0:100]
+filtered_display = remap.density(filtered_image)
+
+print("phase data, with deweighting, dim=0")
+plt.imshow(phase_display)
+plt.show()
+
+print("filtered image")
+plt.imshow(filtered_display)
+plt.show()
+
+aperture_filter = ApertureFilter(reader, dimension=1)
+aperture_filter.set_sub_image_bounds((y_start, y_end), (x_start, x_end))
+aperture_filter.apply_deweighting = False
+phase_data = aperture_filter.normalized_phase_history
+phase_display = remap.density(phase_data)
+filtered_image = aperture_filter[:, 0:100]
+filtered_display = remap.density(filtered_image)
+
+print("phase data, no deweighting, dim=1")
+plt.imshow(phase_display)
+plt.show()
+
+print("filtered image")
+plt.imshow(filtered_display)
+plt.show()
+
+aperture_filter = ApertureFilter(reader, dimension=1)
+aperture_filter.set_sub_image_bounds((y_start, y_end), (x_start, x_end))
+aperture_filter.apply_deweighting = True
+phase_data = aperture_filter.normalized_phase_history
+phase_display = remap.density(phase_data)
+filtered_image = aperture_filter[:, 0:100]
+filtered_display = remap.density(filtered_image)
+
+print("phase data, with deweighting, dim=1")
+plt.imshow(phase_display)
+plt.show()
+
+print("filtered image")
+plt.imshow(filtered_display)
 plt.show()
