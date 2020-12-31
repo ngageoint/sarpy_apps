dd69698132bc8c2490b494b03327f9b3f32b356a
Thomas McCullough <thomas.mccullough@gmail.com>
2020-12-31 12:39:09 -0500

    Adding some common use file filter options

--- sarpy_apps/supporting_classes/file_filters.py
+++ sarpy_apps/supporting_classes/file_filters.py
@@ -25,13 +25,13 @@ def _include_uppercase(filter_list):
         if u_vers not in out:
             out.append(u_vers)
 
-    out = []
-
     if isinstance(filter_list, str):
-        add_element(filter_list)
-    else:
-        for entry in filter_list:
-            add_element(entry)
+        filter_list = filter_list.strip().split()
+
+    out = []
+    for entry in filter_list:
+        add_element(entry)
+
     if len(out) == 1:
         return out[0]
     else:
@@ -40,9 +40,9 @@ def _include_uppercase(filter_list):
 
 all_files = ('ALL Files', '*')
 # sar type images
-nitf_files = ('NITF Files', _include_uppercase(['.ntf', '.nitf']))
-hdf5_files = ('HDF5 Files', _include_uppercase(['.hdf', '.h5', '.hdf5', '.he5']))
-tiff_files = ('TIFF Files', _include_uppercase(['.tiff', '.tif']))
+nitf_files = ('NITF Files', _include_uppercase('.ntf .nitf'))
+hdf5_files = ('HDF5 Files', _include_uppercase('.hdf .h5 .hdf5 .he5'))
+tiff_files = ('TIFF Files', _include_uppercase('.tiff .tif'))
 sar_images = ('SAR Images', nitf_files[1] + hdf5_files[1] + tiff_files[1])
 # json files
 json_files = ('JSON Files', _include_uppercase('.json'))
