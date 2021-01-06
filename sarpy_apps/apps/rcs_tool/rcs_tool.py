2a23c5d6f740d2a1ecc36a42f1dc020654083b89
Thomas McCullough <thomas.mccullough@gmail.com>
2021-01-06 12:53:30 -0500

    Incremental change for rcs_tool

--- sarpy_apps/apps/rcs_tool/rcs_tool.py
+++ sarpy_apps/apps/rcs_tool/rcs_tool.py
@@ -1,157 +1,165 @@
 import os
 
 import numpy
-
 import tkinter
 from tkinter.filedialog import askopenfilename
-from tkinter import Menu
+
 from tk_builder.panel_builder import WidgetPanel
 from tk_builder.panels.image_panel import ImagePanel
 
 from tk_builder.widgets import widget_descriptors
 from tk_builder.widgets import basic_widgets
 from tk_builder.panel_builder import RadioButtonPanel
+from tk_builder.widgets.image_canvas import ToolConstants
 
 from sarpy_apps.supporting_classes.image_reader import ComplexImageReader
+from sarpy_apps.supporting_classes import file_filters
 
-from tk_builder.widgets.image_canvas import ToolConstants
 from sarpy_apps.apps.rcs_tool.popups.rcs_plot import RcsPlot
 
 __classification__ = "UNCLASSIFIED"
 __author__ = "Jason Casey"
 
 
+###########
+# associated GUI elements
+
+class RoiRadiobuttons(RadioButtonPanel):
+    _widget_list = ("rectangle", "polygon",)
+    rectangle = widget_descriptors.RadioButtonDescriptor("rectangle")  # type: basic_widgets.RadioButton
+    polygon = widget_descriptors.RadioButtonDescriptor("polygon")  # type: basic_widgets.RadioButton
+    ellipse = widget_descriptors.RadioButtonDescriptor("ellipse")  # type: basic_widgets.RadioButton
+
+    def __init__(self, parent):
+        RadioButtonPanel.__init__(self, parent)
+        self.parent = parent
+        self.init_w_vertical_layout()
+
+
+class RoiControls(WidgetPanel):
+    _widget_list = ("roi_radiobuttons", "draw", "edit", "delete")
+    roi_radiobuttons = widget_descriptors.PanelDescriptor(
+        "roi_radiobuttons", RoiRadiobuttons)  # type: RoiRadiobuttons
+    draw = widget_descriptors.ButtonDescriptor("draw")  # type: basic_widgets.Button
+    edit = widget_descriptors.ButtonDescriptor("edit")  # type: basic_widgets.Button
+    delete = widget_descriptors.ButtonDescriptor("delete")  # type: basic_widgets.Button
+
+    def __init__(self, parent):
+        WidgetPanel.__init__(self, parent)
+        self.parent = parent
+        self.init_w_vertical_layout()
+
+
+class DataGenerationOptions(WidgetPanel):
+    class MeasureOptions:
+        rcs = "RCS"
+        sigma_0 = "Sigma-0"
+        beta_0 = "Beta-0"
+        gamma_0 = "Gamma-0"
+        avg_pixel_power = "avg. Pixel Power"
+
+    class SlowTimeUnits:
+        azimuth_angle = "Azimuth Angle"
+        aperture_relative = "Aperture Relative"
+        collect_time = "Collect Time"
+        polar_angle = "Polar Angle"
+        target_relative = "Target Relative"
+
+    _widget_list = (
+        "measure_label", "measure_dropdown", "slow_time_units",
+        "slow_time_dropdown", "target_azimuth_label", "target_azimuth",
+        "use_cal_constant", "db", "db_label", "plot_in_db", "exclude_zeropad")
+
+    measure_label = widget_descriptors.LabelDescriptor(
+        "measure_label", default_text="Measure: ")  # type: basic_widgets.Label
+    measure_dropdown = widget_descriptors.ComboboxDescriptor(
+        "measure_dropdown")  # type: basic_widgets.Combobox
+
+    slow_time_units = widget_descriptors.LabelDescriptor(
+        "slow_time_units", default_text="Slow-Time Units: ")  # type: basic_widgets.Label
+    slow_time_dropdown = widget_descriptors.ComboboxDescriptor(
+        "slow_time_dropdown")  # type: basic_widgets.Combobox
+
+    target_azimuth_label = widget_descriptors.LabelDescriptor(
+        "target_azimuth_label", default_text="Target Azimuth")  # type: basic_widgets.Label
+    target_azimuth = widget_descriptors.EntryDescriptor(
+        "target_azimuth", default_text="0")  # type: basic_widgets.Entry
+
+    use_cal_constant = widget_descriptors.CheckButtonDescriptor(
+        "use_cal_constant", default_text="Use cal constant")  # type: basic_widgets.Combobox
+    db = widget_descriptors.EntryDescriptor(
+        "db", default_text="0")  # type: basic_widgets.Entry
+    db_label = widget_descriptors.LabelDescriptor(
+        "db_label", default_text="db")  # type: basic_widgets.Label
+
+    plot_in_db = widget_descriptors.CheckButtonDescriptor(
+        "plot_in_db", default_text="Plot in dB")  # type: basic_widgets.CheckButton
+    exclude_zeropad = widget_descriptors.CheckButtonDescriptor(
+        "exclude_zeropad", default_text="Exclude zeropad")  # type: basic_widgets.CheckButton
+
+    def __init__(self, parent):
+        WidgetPanel.__init__(self, parent)
+        self.parent = parent
+        self.init_w_basic_widget_list(5, [2, 2, 2, 3, 2])
+
+        self.measure_dropdown.pack(expand=tkinter.YES, fill=tkinter.X)
+        self.slow_time_dropdown.pack(expand=tkinter.YES, fill=tkinter.X)
+        self.target_azimuth.pack(expand=tkinter.YES, fill=tkinter.X)
+        self.db.pack(expand=tkinter.YES, fill=tkinter.X)
+
+        self.measure_dropdown.update_combobox_values([self.MeasureOptions.rcs,
+                                                      self.MeasureOptions.sigma_0,
+                                                      self.MeasureOptions.beta_0,
+                                                      self.MeasureOptions.gamma_0,
+                                                      self.MeasureOptions.avg_pixel_power])
+
+        self.slow_time_dropdown.update_combobox_values([self.SlowTimeUnits.azimuth_angle,
+                                                        self.SlowTimeUnits.aperture_relative,
+                                                        self.SlowTimeUnits.collect_time,
+                                                        self.SlowTimeUnits.polar_angle,
+                                                        self.SlowTimeUnits.target_relative])
+
+
+class PlotButtons(WidgetPanel):
+    _widget_list = (
+        "plot", "save_kml", "image_profile", "save_as_text")
+    plot = widget_descriptors.ButtonDescriptor("plot", default_text="Plot")  # type: basic_widgets.Button
+    save_kml = widget_descriptors.ButtonDescriptor("save_kml", default_text="Save KML")  # type: basic_widgets.Button
+    image_profile = widget_descriptors.ButtonDescriptor("image_profile", default_text="Image Profile")  # type: basic_widgets.Button
+    save_as_text = widget_descriptors.ButtonDescriptor("save_as_text", default_text="Save As Text")  # type: basic_widgets.Button
+
+    def __init__(self, parent):
+        WidgetPanel.__init__(self, parent)
+        self.parent = parent
+        self.init_w_box_layout(2, 10, 1)
+
+
 class ControlsPanel(WidgetPanel):
-    class RoiControls(WidgetPanel):
-        class RoiRadiobuttons(RadioButtonPanel):
-            _widget_list = ("rectangle", "polygon",)
-            rectangle = widget_descriptors.RadioButtonDescriptor("rectangle")  # type: basic_widgets.RadioButton
-            polygon = widget_descriptors.RadioButtonDescriptor("polygon")  # type: basic_widgets.RadioButton
-            ellipse = widget_descriptors.RadioButtonDescriptor("ellipse")  # type: basic_widgets.RadioButton
-
-            def __init__(self, parent):
-                RadioButtonPanel.__init__(self, parent)
-                self.parent = parent
-                self.init_w_vertical_layout()
-
-        _widget_list = ("roi_radiobuttons", "draw", "edit", "delete")
-
-        roi_radiobuttons = widget_descriptors.PanelDescriptor("roi_radiobuttons", RoiRadiobuttons)  # type: ControlsPanel.RoiControls.RoiRadiobuttons
-        draw = widget_descriptors.ButtonDescriptor("draw")  # type: basic_widgets.Button
-        edit = widget_descriptors.ButtonDescriptor("edit")  # type: basic_widgets.Button
-        delete = widget_descriptors.ButtonDescriptor("delete")  # type: basic_widgets.Button
-
-        def __init__(self, parent):
-            WidgetPanel.__init__(self, parent)
-            self.parent = parent
-            self.init_w_vertical_layout()
-
-    class DataGenerationOptions(WidgetPanel):
-        _widget_list = ("measure_label", "measure_dropdown",
-                        "slow_time_units", "slow_time_dropdown",
-                        "target_azimuth_label", "target_azimuth",
-                        "use_cal_constant", "db", "db_label",
-                        "plot_in_db", "exclude_zeropad")
-
-        measure_label = widget_descriptors.LabelDescriptor(
-            "measure_label", default_text="Measure: ")  # type: basic_widgets.Label
-        measure_dropdown = widget_descriptors.ComboboxDescriptor(
-            "measure_dropdown")  # type: basic_widgets.Combobox
-
-        slow_time_units = widget_descriptors.LabelDescriptor(
-            "slow_time_units", default_text="Slow-Time Units: ")  # type: basic_widgets.Label
-        slow_time_dropdown = widget_descriptors.ComboboxDescriptor(
-            "slow_time_dropdown")  # type: basic_widgets.Combobox
-
-        target_azimuth_label = widget_descriptors.LabelDescriptor(
-            "target_azimuth_label", default_text="Target Azimuth")  # type: basic_widgets.Label
-        target_azimuth = widget_descriptors.EntryDescriptor(
-            "target_azimuth", default_text="0")  # type: basic_widgets.Entry
-
-        use_cal_constant = widget_descriptors.CheckButtonDescriptor(
-            "use_cal_constant", default_text="Use cal constant")  # type: basic_widgets.Combobox
-        db = widget_descriptors.EntryDescriptor(
-            "db", default_text="0")  # type: basic_widgets.Entry
-        db_label = widget_descriptors.LabelDescriptor(
-            "db_label", default_text="db")  # type: basic_widgets.Label
-
-        plot_in_db = widget_descriptors.CheckButtonDescriptor(
-            "plot_in_db", default_text="Plot in dB")  # type: basic_widgets.CheckButton
-        exclude_zeropad = widget_descriptors.CheckButtonDescriptor(
-            "exclude_zeropad", default_text="Exclude zeropad")  # type: basic_widgets.CheckButton
-
-        class MeasureOptions:
-            rcs = "RCS"
-            sigma_0 = "Sigma-0"
-            beta_0 = "Beta-0"
-            gamma_0 = "Gamma-0"
-            avg_pixel_power = "avg. Pixel Power"
-
-        class SlowTimeUnits:
-            azimuth_angle = "Azimuth Angle"
-            aperture_relative = "Aperture Relative"
-            collect_time = "Collect Time"
-            polar_angle = "Polar Angle"
-            target_relative = "Target Relative"
-
-        def __init__(self, parent):
-            WidgetPanel.__init__(self, parent)
-            self.parent = parent
-            self.init_w_basic_widget_list(5, [2, 2, 2, 3, 2])
-
-            self.measure_dropdown.pack(expand=tkinter.YES, fill=tkinter.X)
-            self.slow_time_dropdown.pack(expand=tkinter.YES, fill=tkinter.X)
-            self.target_azimuth.pack(expand=tkinter.YES, fill=tkinter.X)
-            self.db.pack(expand=tkinter.YES, fill=tkinter.X)
-
-            self.measure_dropdown.update_combobox_values([self.MeasureOptions.rcs,
-                                                          self.MeasureOptions.sigma_0,
-                                                          self.MeasureOptions.beta_0,
-                                                          self.MeasureOptions.gamma_0,
-                                                          self.MeasureOptions.avg_pixel_power])
-
-            self.slow_time_dropdown.update_combobox_values([self.SlowTimeUnits.azimuth_angle,
-                                                            self.SlowTimeUnits.aperture_relative,
-                                                            self.SlowTimeUnits.collect_time,
-                                                            self.SlowTimeUnits.polar_angle,
-                                                            self.SlowTimeUnits.target_relative])
-
-    class PlotButtons(WidgetPanel):
-        _widget_list = ("plot", "save_kml", "image_profile", "save_as_text")
-
-        plot = widget_descriptors.ButtonDescriptor("plot", default_text="Plot")  # type: basic_widgets.Button
-        save_kml = widget_descriptors.ButtonDescriptor("save_kml", default_text="Save KML")  # type: basic_widgets.Button
-        image_profile = widget_descriptors.ButtonDescriptor("image_profile", default_text="Image Profile")  # type: basic_widgets.Button
-        save_as_text = widget_descriptors.ButtonDescriptor("save_as_text", default_text="Save As Text")  # type: basic_widgets.Button
-
-        def __init__(self, parent):
-            WidgetPanel.__init__(self, parent)
-            self.parent = parent
-            self.init_w_box_layout(2, 10, 1)
-
     _widget_list = ("roi_controls", "data_generation_options", "plot_buttons")
 
-    roi_controls = widget_descriptors.PanelDescriptor("roi_controls", RoiControls)  # type: ControlsPanel.RoiControls
-    data_generation_options = widget_descriptors.PanelDescriptor("data_generation_options", DataGenerationOptions)  # type: ControlsPanel.DataGenerationOptions
-    plot_buttons = widget_descriptors.PanelDescriptor("plot_buttons", PlotButtons)  # type: ControlsPanel.PlotButtons
+    roi_controls = widget_descriptors.PanelDescriptor("roi_controls", RoiControls)  # type: RoiControls
+    data_generation_options = widget_descriptors.PanelDescriptor("data_generation_options", DataGenerationOptions)  # type: DataGenerationOptions
+    plot_buttons = widget_descriptors.PanelDescriptor("plot_buttons", PlotButtons)  # type: PlotButtons
 
     def __init__(self, parent):
         WidgetPanel.__init__(self, parent)
         self.init_w_vertical_layout()
 
 
-class RcsTable(WidgetPanel):
-    class Buttons(WidgetPanel):
-        _widget_list = ("edit", )
-        edit = widget_descriptors.ButtonDescriptor("edit")  # type: basic_widgets.Button
+class Buttons(WidgetPanel):
+    _widget_list = ("edit", )
+    edit = widget_descriptors.ButtonDescriptor("edit")  # type: basic_widgets.Button
 
-        def __init__(self, parent):
-            WidgetPanel.__init__(self, parent)
-            self.init_w_horizontal_layout()
+    def __init__(self, parent):
+        WidgetPanel.__init__(self, parent)
+        self.init_w_horizontal_layout()
+
+
+class RcsTable(WidgetPanel):
 
     _widget_list = ("table", "buttons", )
     table = widget_descriptors.TreeviewDescriptor("table")  # type: basic_widgets.Treeview
-    buttons = widget_descriptors.PanelDescriptor("buttons", Buttons)  # type: RcsTable.Buttons
+    buttons = widget_descriptors.PanelDescriptor("buttons", Buttons)  # type: Buttons
 
     def __init__(self, parent):
         WidgetPanel.__init__(self, parent)
@@ -185,8 +193,6 @@ class RcsTable(WidgetPanel):
 
 
 class RcsTool(WidgetPanel):
-    __slots__ = (
-        '_sicd_reader', '_color_cycler')
     _widget_list = ("controls",  "image_panel", "rcs_table",)
 
     controls = widget_descriptors.PanelDescriptor("controls", ControlsPanel)  # type: ControlsPanel
@@ -194,10 +200,8 @@ class RcsTool(WidgetPanel):
     rcs_table = widget_descriptors.PanelDescriptor("rcs_table", RcsTable, default_text="RCS Table")  # type: RcsTable
 
     def __init__(self, primary):
-
         # define variables
-        self.sicd_reader = None
-
+        self._browse_directory = os.path.expanduser('~')
         self.primary = primary
         primary_frame = tkinter.Frame(primary)
 
@@ -207,9 +211,8 @@ class RcsTool(WidgetPanel):
         self.controls.pack(fill=tkinter.X, expand=False)
         self.rcs_table.pack(fill=tkinter.X, expand=True)
 
-        menubar = Menu()
-
-        filemenu = Menu(menubar, tearoff=0)
+        menubar = tkinter.Menu()
+        filemenu = tkinter.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open", command=self.select_file)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.exit)
@@ -272,7 +275,7 @@ class RcsTool(WidgetPanel):
             else:
                 pass
 
-        if self.image_panel.variables.current_shape_id is not None:
+        if self.image_panel.canvas.variables.current_shape_id is not None:
             self.rcs_table.table.selection_set(self.image_panel.canvas.variables.current_shape_id)
 
     # commands for controls.plot_buttons
@@ -292,7 +295,7 @@ class RcsTool(WidgetPanel):
 
     def edit_rcs_table(self):
         current_item = self.rcs_table.table.focus()
-        print(self.rcs_table.table.item(current_item))
+        print(self.rcs_table.table.item(current_item))  # TODO: what is this for?
 
     def set_tool(self):
         if self.controls.roi_controls.roi_radiobuttons.selection() == self.controls.roi_controls.roi_radiobuttons.rectangle:
@@ -312,17 +315,33 @@ class RcsTool(WidgetPanel):
             self.rcs_table.delete(current_shape_id)
 
     def select_file(self):
-        fname_filters = [("NITF", ".nitf .NITF .ntf .NTF"), ('All files', '*')]
-        fname = askopenfilename(initialdir=os.path.expanduser("~"), filetypes=fname_filters)
-        self.sicd_reader = ComplexImageReader(fname)
-        self.image_panel.set_image_reader(self.sicd_reader)
+        fname = askopenfilename(initialdir=self._browse_directory, filetypes=file_filters.common_use_filter)
+        if fname is None or fname == '':
+            return
+        self._browse_directory = os.path.split(fname)[0]
+        # TODO: what all file types to support? Create a more common use way of dealing with this.
+        reader = ComplexImageReader(fname)
+
+        self.image_panel.set_image_reader(reader)
         self.image_panel.update_everything()
 
     def exit(self):
         self.quit()
 
 
-if __name__ == '__main__':
+def main():
+    """
+    Run a RcsTool application.
+
+    Returns
+    -------
+    None
+    """
+
     root = tkinter.Tk()
     app = RcsTool(root)
     root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
