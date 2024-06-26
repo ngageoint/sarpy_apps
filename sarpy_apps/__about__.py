6b1acba260e4219e48e099447275fda927436879
pressler-vsc <78438270+pressler-vsc@users.noreply.github.com>
2024-06-26 08:13:00 -0700

    Update __about__.py (#56)
    Changed email address for contact requests (Wade->Rick).

--- sarpy_apps/__about__.py
+++ sarpy_apps/__about__.py
@@ -31,7 +31,7 @@ __version__ = "1.1.23"
 __classification__ = "UNCLASSIFIED"  # This should be set appropriately in any high-side version
 __author__ = "National Geospatial-Intelligence Agency"
 __url__ = "https://github.com/ngageoint/sarpy_apps"
-__email__ = "Wade.C.Schwartzkopf@nga.mil"
+__email__ = "richard.m.naething@nga.mil"
 
 
 __title__ = "sarpy_apps"
